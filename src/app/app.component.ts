a5dc02c5ee050fb00db19701671bb0868d5311f6
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-07-31 15:21:40 +0200

    install firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDdy23shagXwTfOBD6vWgJla8ES2NJ_cCY",
+      authDomain: "bookshelves-c1ea8.firebaseapp.com",
+      databaseURL: "https://bookshelves-c1ea8.firebaseio.com",
+      projectId: "bookshelves-c1ea8",
+      storageBucket: "",
+      messagingSenderId: "26978152049",
+      appId: "1:26978152049:web:2403a49548cc4060"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
